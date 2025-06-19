dfe534b1b3dec3a3804a9eab4d61840419bf6cdf
Pedro Luis <pedroluisnr22@gmail.com>
2025-06-18 23:28:51 -0500

    build: :wrench: add default port and secret

--- src/main/kotlin/com/github/pedroluis02/springsecuritysample/security/UserDetailsServiceImpl.kt
+++ src/main/kotlin/com/github/pedroluis02/springsecuritysample/security/UserDetailsServiceImpl.kt
@@ -5,7 +5,6 @@ import com.github.pedroluis02.springsecuritysample.domain.service.UserService
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.security.core.userdetails.User as UserDetailsImpl
 
